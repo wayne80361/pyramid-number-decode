006c9094683c9a3ed578caf78da3d4deebebdb30
wayne80361 <wayne80361@gmail.com>
2024-04-16 16:11:52 -0400

    add a js file with some pseudo code

---
+++ pyramidpattern.js
@@ -0,0 +1,10 @@
+// given a number array and turn it into a pyramid
+// example, [1,2,3,4,5,6] will turn into
+// [[1],[2,3],[4,5,6]]
+
+// not sorted but still increment by 1 from 1
+// example, [2,4,1,7,6,5,3,9,8,10] will turn into
+// [[1],[2,3],[4,5,6],[7,8,9,10]]
+
+// each number will pair with a word
+// figure only the word on the right from top to bottom
